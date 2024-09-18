7d0513d9b223f804b6ce9aab0b3867de9ecbd574
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2024-09-17 22:23:30 -0300

    javascript
    Nesse comit fiz a parte um do javasript

---
+++ script.js
@@ -0,0 +1,34 @@
+const guessSection = document.querySelector("#guess-section");
+const guessInput = document.querySelector("#guess");
+const guessButton = document.querySelector("#guess-btn");
+const resultParagraph = document.querySelector("#result");
+const dificultySelect = document.querySelector("#dificulty");
+const dificultySection = document.querySelector("#dificulty-section");
+const gameSection = document.querySelector("#game-section");
+const triesLeftSpan = document.querySelector("#tries-span");
+const resetButton = document.querySelector("#reset-btn");
+
+let maxtries;
+let randomNumber;
+let triesLeft;
+
+dificultySelect.addEventListener("change", function(){
+
+    const dificulty = parseInt(dificultySelect.value);
+
+    switch (dificulty){
+
+        case 1: 
+            maxtries = 10; 
+            break; 
+        case 2 :
+            maxtries = 7;
+            break;
+        case 3:
+            maxtries = 5;
+            break;
+        default:
+            maxtries = 10;
+            break;
+    }
+})
